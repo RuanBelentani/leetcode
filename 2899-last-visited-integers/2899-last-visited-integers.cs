10d888909ae6809f1213f7d4056e58900cdedc38
Ruan Belentani <103147704+RuanBelentani@users.noreply.github.com>
2024-05-08 16:38:13 -0300

    Time: 122 ms (5.26%), Space: 48.4 MB (13.16%) - LeetHub

---
+++ 2899-last-visited-integers/2899-last-visited-integers.cs
@@ -0,0 +1,37 @@
+public class Solution {
+    public IList<int> LastVisitedIntegers(int[] nums) {
+        int[] seen = new int[nums.Length];
+        List<int> ans = new List<int>();
+        int aux;
+        int lastNumber = 0, seenCount = 0, k = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] > 0)
+            {
+                int reservedNumber = nums[i];
+                for (var j = 0; j < seen.Length; j++)
+                {
+                    aux = reservedNumber;
+                    reservedNumber = seen[j];
+                    seen[j] = aux;
+                }
+                seenCount++;
+            }
+            else
+            {
+                if (lastNumber == -1)
+                    k++;
+                else
+                    k = 1;
+                
+                if (k > seenCount)
+                    ans.Add(-1);
+                else
+                    ans.Add(seen[k-1]);
+            }
+            lastNumber = nums[i];
+        }
+        return ans;
+    }
+}
