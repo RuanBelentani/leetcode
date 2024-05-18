2602175f6164b3fc76af234267f4adc9549a524b
Ruan Belentani <103147704+RuanBelentani@users.noreply.github.com>
2024-05-18 08:40:07 -0300

    Time: 155 ms (15.54%), Space: 47.3 MB (82.00%) - LeetHub

--- 0001-two-sum/0001-two-sum.cs
+++ 0001-two-sum/0001-two-sum.cs
@@ -2,7 +2,7 @@ public class Solution {
     public int[] TwoSum(int[] nums, int target) {    
         for (var i = 0; i < nums.Length -1; i++)
         {
-            for (var j = 0; j < nums.Length; j++)
+            for (var j = 1; j < nums.Length; j++)
             {
                 if ((nums[i] + nums[j] == target) && (i != j))
                     return new int[]{i, j};
