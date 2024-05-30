099352cbd5c18aaaaa3dfd1d09ed978ea942650d
Ruan Belentani <103147704+RuanBelentani@users.noreply.github.com>
2024-05-30 07:21:36 -0300

    Time: 48 ms (57.99%), Space: 32.8 MB (62.67%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public bool IsPalindrome(int x) {
+        if (x < 0)
+            return false;
+        
+        string number = Convert.ToString(x);
+        
+/*        if (number.Length % 2 == 0)
+            return false;        */
+        
+        for(int i = 0, j = number.Length -1; i < number.Length / 2; i++,j--)
+        {
+            if (number[i] != number[j])
+                return false;
+        }
+        
+        return true;        
+    }
+}
